d64a2edcbf0ba2714454f9ccf12daa5ade8c031d
Bibizaynap <104783702+Bibizaynap@users.noreply.github.com>
2022-12-16 22:31:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const card = document.querySelectorAll(".card__inner");
+
+card.forEach((item, i) =>  {
+    item.addEventListener('click', () => {
+        item.classList.toggle('is-flipped');
+    })
+})
